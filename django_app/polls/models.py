2da773701736aa21b3fcc25d67d408ae152b70a8
Joonmo Kim <kizmo04@Joonmoui-MacBook-Pro.local>
2017-02-06 12:58:35 +0900

    first commit

---
+++ django_app/polls/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+class Question(models.Model):
+    q_text = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    c_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+git re
